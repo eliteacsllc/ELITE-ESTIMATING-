f98f82f409041ce2a8a63a9816c44cf33bbd215c
eliteacsllc <eliteacsllc@gmail.com>
2026-08-23 05:36:34 -0400

    Export operational health metrics

--- src/index.ts
+++ src/index.ts
@@ -37,3 +37,4 @@ export * from './damage/service.js';
 export * from './property/measurement.js';
 export * from './property/scope.js';
 export * from './observability/metrics.js';
+export * from './observability/operational.js';
